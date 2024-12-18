85197b4ea9aa9e53d245b6c7ef5d62bdcab684f2
Klemensmandzaro <58343390+Klemensmandzaro@users.noreply.github.com>
2024-12-18 15:23:55 +0100

    Add files via upload

--- calculator/calculator.asm
+++ calculator/calculator.asm
@@ -7,27 +7,41 @@ org 100h
 mov BX, liczba+4
 mov CX, 0
 mov DX, 10
-;naprawic
+mov byte [licznik], 0
+mov byte [licznik2], 0
+mov AX, 0
+
 
 petla:
 	dec BX
+	mov AX, 0
 	mov AL, [BX]
 	sub AL, 30h
-	MUL DL
+	mov BP, [licznik2]
+	cmp BP, [licznik]
+JG mnozenie
+petla2:
+	inc byte [licznik2]
 	add CX, AX
-	mov AX, 0xA
-	MUL DX
-	mov DX, AX
+	mov byte [licznik], 0
 	cmp BX, liczba
 JG petla
+mov word [liczba], CX
+jmp koniec
 
-
+mnozenie:
+		mul DX
+		mov DX, 10
+		inc byte [licznik]
+		cmp BP, [licznik]
+JG mnozenie
+JE petla2
 
 ;mov liczba, BX
+koniec:
+	mov AH, 00h
+	int 21h
 
-mov AH, 00h
-int 21h
-
-
-
+licznik2 dw 1
+licznik dw 1
 liczba db "9570"
